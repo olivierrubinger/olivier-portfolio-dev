ad2daa30b9c4ba6472ebe4b3d93a7df13c1356f7
Olivier Lopes Rubinger <olivierrubinger@gmail.com>
2024-04-30 19:46:45 -0300

    Commit Final #1

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -12,6 +12,13 @@ export class ExperiencesComponent {
   public arrayExperiences = signal<IExperiences[]>([
     {
       summary: {
+        strong: 'Junior Developer TJPA',
+        p: 'Qintess | Apr 2024 - Present',
+      },
+      text: '<p>As a Junior Developer at the Tribunal de Justiça do Estado do Pará (TJPA), I am actively engaged in crafting dynamic web applications and solutions using Angular and Java. Within our development team, I collaborate closely with experienced developers and project managers to deliver robust software applications that enhance the efficiency and functionality of judiciary processes.</p>',
+    },
+    {
+      summary: {
         strong: 'Junior Developer Full-Stack',
         p: 'Cert One | Apr 2023 - May 2023',
       },
@@ -29,7 +36,7 @@ export class ExperiencesComponent {
         strong: 'Full-stack Freelancer',
         p: 'Self-employed | Aug 2022 - Present',
       },
-      text: "<p>As a Full-stack Freelancer, I`ve created web applications for companies exploring their needs. I helped programming teams with development and learned a lot by myself.</p>",
+      text: '<p>As a Full-stack Freelancer, I`ve created web applications for companies exploring their needs. I helped programming teams with development and learned a lot by myself.</p>',
     },
     {
       summary: {
--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -17,13 +17,18 @@ export class ProjectsComponent {
 
   public arrayProjects = signal<IProjects[]>([
     {
-      src: 'assets/img/projects/vfull.png',
+      src: 'assets/img/projects/portfolio.jpg',
       alt: 'Projeto',
-      title: 'Vida Fullstack',
-      width: '100px',
-      height: '51px',
-      description: 'oioioioioi',
-      links: [{ name: 'Conheça o Blog', href: 'https://vidafullstack.com.br' }],
+      title: "Olivier Rubinger's Portfolio",
+      width: '120px',
+      height: '120px',
+      description: "This website's Github respository!",
+      links: [
+        {
+          name: 'Go to Github',
+          href: 'https://github.com/olivierrubinger/olivier-portfolio-dev',
+        },
+      ],
     },
   ]);
 
